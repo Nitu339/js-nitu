7c2968069f83a2d017a147293d6739d1c4cb6712
Nitu339 <66820334+Nitu339@users.noreply.github.com>
2026-01-19 15:52:11 +0000

    container config done

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,27 @@
+"use strict";
+//alert(3+3)
+console.log(3+
+    3)//code readibiltiy should be high
+
+    //Data types
+
+    let studentName="Hitesh"
+    let age=18
+    let isLoggedIn=true
+    let state;
+    console.log(typeof studentName)
+    console.log(typeof age)
+    console.log(typeof isLoggedIn)
+    console.log(typeof state)
+    console.log(typeof undefined)
+    console.log(typeof null)
+
+    /*
+    number
+    bigint
+    string
+    boolean
+    null=> standalone value
+    undefined=> undefined (typeof)
+    symbol=>unique
+    */
