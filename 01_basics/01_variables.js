13678f9db08b829aca8db4c2ed446edbc2c7c987
Nitu339 <66820334+Nitu339@users.noreply.github.com>
2026-01-19 15:35:34 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+let Accountname="Nitu"
+const password=12345
+//password=123456 not allowed we can't modidy the const variables
+let email="nitu@gmail.com"
+city="kolkata"
+
+/*console.log(Accountname)
+console.log(password)
+console.log(email)
+console.log(city)*/
+
+console.table([Accountname,password,email,city])
+
+/*
+ we prefer not to use var to declare any variables because of issue in block scope and functional scope
+var job="teacher"
+var job="doctor"
+console.log(job)*/
