695f081120e3719ee8642ab7e1296be9439cb777
Nitu339 <66820334+Nitu339@users.noreply.github.com>
2026-01-22 15:33:09 +0000

    container config done

---
+++ 01_basics/operators.js
@@ -0,0 +1,81 @@
+//Arithmetic operators
+/*let a=15;
+let b=10;
+
+console.log("a=",a,"& b=",b);
+console.log("a + b =",a+b);
+console.log("a - b =",a-b);
+console.log("a * b =",a*b);
+console.log("a / b =",a/b);
+console.log("a % b =",a%b);
+console.log("a ** b =",a**b);*/
+
+//unary operator
+
+/*let a=5;
+let b=2;
+
+console.log("a=", a, "& b=",b);
+++a;
+console.log("a=",a);
+a++;
+console.log("a=",a);
+a--;
+console.log("a=",a);
+--a;
+console.log("a=",a);
+b++;
+console.log("b=",b);
+b--;
+console.log("b=",b);*/
+
+//Assignment operators
+
+/*let a=5;
+let b=4;
+
+a+=4 //a=a+4
+console.log(a);
+a-=2
+console.log(a);
+a*=4
+console.log(a)
+a/=7;
+console.log(a);
+a**=4
+console.log(a);
+*/
+
+//Comparison operators
+
+/*let a=5;
+let b=3;
+
+console.log("5==2",a==b);
+console.log("5!=2",a!=b);*/
+
+/*let a=5;
+let b="5";
+
+console.log(a==b);
+console.log(a!=b);*/
+
+//In order to avoid string and number conversion we will be using ===
+
+let a=5;
+let b="5";
+
+console.log(a===b);
+console.log(a!==b);
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 01_basics/practice1.js
@@ -0,0 +1,32 @@
+//create an object
+
+/*const product={
+    title: "Ball pen",
+    rating:4,
+    offer:5,
+    price:270,
+
+
+};*/
+//console.log(product);
+
+/*const profile={
+    username:"Nitu345kum",
+    followers:123,
+    isfollow:true,
+    following:50,
+
+
+};*/
+//console.log(profile);
+
+const dietmeal={
+    morningdrink:"jeera-lemon shot",
+    breakfast:"boiled eggs",
+    midbreakfast:"Any seaonal fruit",
+    lunch:"1 bowl rice dal salad",
+    dinner:"2 roti sbji ",
+};
+console.log(dietmeal);
+
+
