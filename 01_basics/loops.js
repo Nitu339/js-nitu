8289173d9fca4d915ba442e586e4f0f2d4a1f68d
Nitu339 <66820334+Nitu339@users.noreply.github.com>
2026-01-28 14:22:33 +0000

    container config done

---
+++ 01_basics/loops.js
@@ -0,0 +1,45 @@
+//loops are used to execute a piece of code again and again
+
+//for loop
+
+/*for(let i=1;i<=5;i++){
+    console.log("Apna College");
+    
+}*/
+
+//calculate sum of 1 to 5
+/*let sum=0;
+let n=100;
+
+for(let i=1;i<=n;i++)
+{
+sum=sum+i;
+}
+console.log("sum=",sum);
+console.log("loop has ended");
+*/
+
+/*for(let i=1;i<=5;i++){
+    console.log("i=",i);
+    
+}*/
+
+
+
+//while loop
+/*let i=1;
+while(i<=10){
+    console.log("Maa");
+    i++;
+}*/
+
+//do-while loop
+
+let i=8
+;
+
+do{
+    console.log("Apna  college");
+    i++;
+    
+}while(i<=10);
