0d3e7570fa84b14d8fc050eddd0495896c9c08eb
Nitu339 <66820334+Nitu339@users.noreply.github.com>
2026-01-31 15:28:03 +0000

    container config done

---
+++ 01_basics/str.js
@@ -0,0 +1,81 @@
+/*let str="Maa Enterprises"
+let str2="Rajeev Nayan"
+//to print the string value
+//console.log(str);
+//console.log(str2);
+
+//to know the length of string
+//console.log(str.length);
+//console.log(str2.length);
+
+console.log(str[2],str[5],str[7]);*/
+
+/*let obj={
+    item:"pen",
+    price:10,
+    color:"black",
+};
+
+*/
+//console.log("The cost of",obj.item,"is",obj.price,"rupees");
+//console.log("And the color of the pen is",obj.color);
+
+//string method in js
+
+
+//let str="Maa Enterprises "
+ //console.log(str.toUpperCase());
+//console.log(str.toLowerCase());
+//console.log(str.trim()); // removes the extra whitespaces
+
+//console.log(str.slice(2,7));
+
+//str.slice(start,end) returns the part of the string
+//str.concat(str2) // to join the string
+//str.replace(search val,new val)
+//str.charAt(idx)
+
+/*let str="Nitu Kumari"
+let str2="Rajeev Nayan"
+
+//console.log(str.replaceAll("N","R"));
+//console.log(str2.replace("N","n"));
+
+//console.log(str.slice(0,4));
+//console.log(str.concat(str2));
+console.log(str.charAt(0));
+console.log(str2.charAt(0));
+
+
+
+
+
+
+
+
+
+
+//let res="hello" +" Nitu";
+//console.log(res);*/
+
+/*let fullName="Nitukumari";
+let userName="@" +fullName+fullName.length;
+console.log(userName);*/
+
+let Name="Nitu ";
+let name2=" Rajeev";
+
+let res=Name+"Loves"+name2;
+console.log(res);
+
+
+
+
+
+
+
+
+
+
+
+
